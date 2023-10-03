f62b2aeb86ce7cbfe5604f33664de6233daf512f
009kings <009kings@gmail.com>
2023-10-03 12:03:27 -0700

    timers and resets and functions oh my

--- main.js
+++ main.js
@@ -35,28 +35,103 @@ resetButton.addEventListener('click', handleResetClick);
 
 // ---------------------------------------
 let boxes = document.querySelectorAll('.box');
+let jsBoxes = [];
+let timerCount = 0;
+let intervalId = null;
 
-// iterating over the boxes
-for (let i = 0; i < boxes.length; i++) {
-  console.log(boxes[i])
-
-  boxes[i].addEventListener('click', (e) => {
-    // toggle between hotpink and no color
-    // if background is not hotpink, 
-      // make it hotpink
-    // else
-      //  make background ''
-    
-    // if (boxes[i].style.background != 'hotpink') {
-    //   boxes[i].style.background = 'hotpink'
-    // } else {
-    //   boxes[i].style.background = ''
-    // }
-
-    if (boxes[i].classList.contains('hotpink')) {
-      boxes[i].classList.remove('hotpink');
-    } else {
-      boxes[i].classList.add('hotpink');
+let checkWinCondition = () => {
+  // iterate through my jsBoxes array and check if all isPink = true
+  for (let i = 0; i < jsBoxes.length; i++) {
+    // return false if even one is not pink
+    if (jsBoxes[i].isPink == false) {
+      return false;
     }
-  })
+  }
+  // return true if allare pink
+  return true;
 }
+
+let handleWin = () => {
+  // Make each box unclickable
+  for (let i = 0; i < jsBoxes.length; i++) {
+    jsBoxes[i].domElement.removeEventListener('click', handleClick);
+    jsBoxes[i].isPink = false;
+  }
+
+  // stop the timer
+  clearInterval(intervalId);
+
+  // add a message
+  document.querySelector('#box-message').textContent = 'All boxes are pink!'
+}
+
+let handleClick = (event) => {
+  // jsBoxes[i] = {
+  //    domElement: HTMLnode,
+  //    isPink: BOOLEAN
+  //  }
+  
+  // get the index from the DOM so I can reference the JS representation
+  let index = event.target.dataset.index;
+  let box = jsBoxes[index];
+
+  if (box.isPink == true) {
+    box.isPink = false;
+    box.domElement.classList.remove('hotpink');
+  } else {
+    box.isPink = true;
+    box.domElement.classList.add('hotpink');
+  }
+
+  let didWin = checkWinCondition();
+
+  // check winCondition
+  if (didWin == true) {
+    console.log('All boxes are piiink!')
+    handleWin();
+  } else {
+    console.log('make more pink boxes!')
+  }
+}
+
+let handleInterval = () => {
+  // update the game-time seconds
+  timerCount = timerCount + 1;
+
+  // represent that in the dom
+  document.querySelector('#game-time').textContent = timerCount;
+}
+
+let initalizeGame = () => {
+  document.querySelector('#box-message').textContent = ''
+
+  // reset js and dom visual of timer
+  timerCount = 0;
+  document.querySelector('#game-time').textContent = 0;
+  clearInterval(intervalId);
+  
+  // start the timer
+  intervalId = setInterval(handleInterval, 1 * 1000);
+
+  for (let i = 0; i < boxes.length; i++) {
+    // create/add js representation
+    jsBoxes[i] = {
+      domElement: boxes[i],
+      isPink: false
+    }
+  
+    // ensure that all boxes are not hotpink
+    boxes[i].classList.remove('hotpink');
+  
+    // Adding event listenter
+    boxes[i].addEventListener('click', handleClick)
+  }
+}
+
+// setup/initialization
+initalizeGame();
+
+document.querySelector('#box-reset').addEventListener('click', initalizeGame);
+
+// setTimeout
+// match things --> know how to use arrays and objects
